a7baf6c77ee122bb5ff581f0926c6643ad3b810a
RenzoMisago <olympian360@gmail.com>
2025-12-01 09:59:00 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 
-st.title("Gang Logo for Upload")
+st.title("Logo Uploader")
 
 image = Image.open("images/hk-logo.jpg")
 st.image(image, caption="Local Image from Repository", use_column_width=True)
